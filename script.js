32fc7c27e0dca84d275e24fc5ed03c46af1b2dca
sdiai07 <abdullayevasaida32@gmail.com>
2026-03-29 17:40:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert() {
+  alert("Welcome! Let's build something amazing 🚀");
+}
+
+function sendMessage(e) {
+  e.preventDefault();
+  alert("Your message has been sent successfully!");
+}
